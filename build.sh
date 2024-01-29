e47e0c47dccd41214ff7f2960c4158c243d66db3
Shadi Mahameed <shadi.m@dataloop.ai>
2024-01-29 19:06:35 +0200

    Updating docker image

--- build.sh
+++
@@ -1,5 +0,0 @@
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip install -r requirements.txt
-mim install mmengine
-mim install "mmcv>=2.0.0"
-mim install mmdet
